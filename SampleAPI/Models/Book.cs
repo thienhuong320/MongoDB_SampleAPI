0e6d1676ebffcfc10f4c3b734fe840b968df8a88
thienhuong320 <phantranthienhuong320@gmail.com>
2025-01-22 19:48:05 +0700

    first commit

---
+++ SampleAPI/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SampleAPI.Models
+{
+    public class Book
+    {
+        [BsonId] //primary key
+        [BsonRepresentation(BsonType.ObjectId)] //passing paran as string 
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = null!;
+        public string Author { get; set; } = null!;
+    }
+}
